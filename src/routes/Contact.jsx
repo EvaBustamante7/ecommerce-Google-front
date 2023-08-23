2384a9d336317d52aef1e4f6fac5fc43051932b3
Eva Bustamante <evabrodriguez7@gmail.com>
2023-08-23 11:26:47 +0200

    prueba

---
+++ src/routes/Contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Contact_form from '../components/contact_form/Contact_form'
+
+const Contact = () => {
+  return (
+    <Contact_form/>
+  )
+}
+
+export default Contact
