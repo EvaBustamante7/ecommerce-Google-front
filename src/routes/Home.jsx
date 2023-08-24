10cd3bd44c15397785a6e80ce768108eda13c201
Liliana <garciasevericheliliana@gmail.com>
2023-08-24 09:51:19 +0200

    actualizacion de main

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/navbar/Navbar'
+import Banner from '../components/banner/Banner'
 import Footer from '../components/footer/Footer'
 
 const Home = () => {
   return (
     <>
     <Navbar/>
+    <Banner/>
     <Footer/>
     </>
   )
