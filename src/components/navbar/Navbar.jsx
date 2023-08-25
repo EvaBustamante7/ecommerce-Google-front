8c6e974998a0b895a2c86517a080fdbc764d41ea
Eva Bustamante <evabrodriguez7@gmail.com>
2023-08-25 10:22:27 +0200

    navbar component, fix css, add new logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import Logo from '../footer/img/G3DA.svg'
 import './css/Navbar.css'
 
 const Navbar = () => {
+    const[click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+
+    const [color, setColor] = useState(false)
+    const changeColor =() => {
+        if(window.scrollY >= 100 ) {
+            setColor(true)
+        } else {
+            setColor(false)
+        }
+    }
+
+    window.addEventListener('scroll', changeColor)
+
   return (
-   <div className='prueba'>
-    <h1>Prueba Eva</h1>
-    <h2>Soy un subtítulo</h2>   
-    <p>Soy un párrafo kjasajddshflhglkfhglfkhglkfh</p>
-  </div>
+    <div className={color ? 'header header-bg' : 'header'}>
+         <Link to='/'><img id='logo' src={Logo} alt="Logo G3da" /></Link>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li>
+                <Link to='/'>Home</Link>
+            </li>
+            <li>
+                <Link to='/weather'>Weather</Link>
+            </li>
+            <li>
+                <Link to='/productos'>Productos</Link>
+            </li>
+            <li>
+                <Link to='/inmersiva'>Inmersiva</Link>
+            </li>
+            <li>
+                <Link to='/contact'>Contact</Link>
+            </li>
+        </ul>
+        <div className='hamburger' onClick={handleClick}>
+            {click ? (<FaTimes size={20} style={{color: 'white'}} />) : <FaBars size={20} style={{color: 'black'}}/> }
+            
+        </div>
+    </div>
   )
 }
 
