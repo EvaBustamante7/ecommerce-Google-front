9f2da58df9cc9a7a8493d452b1671f9899e6bf26
Liliana <garciasevericheliliana@gmail.com>
2023-08-29 15:16:26 +0200

    modificado el home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Banner from '../components/banner/Banner'
 import Footer from '../components/footer/Footer'
 import Navbar from '../components/navbar/Navbar'
+import Carrusel from '../components/carrusel/Carrusel'
 
 const Home = () => {
   return (
     <>
     <Navbar/>
     <Banner/>
+    <Carrusel/>
     <Footer/>
     </>
   )
