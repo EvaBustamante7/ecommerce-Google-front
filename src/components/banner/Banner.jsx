7691d00b2dcb76c4cff6f8f9e569759467d7e1ff
Liliana <garciasevericheliliana@gmail.com>
2023-08-29 15:07:47 +0200

    banner modificado

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,29 +1,21 @@
 import React from 'react';
 import './css/Banner.css';
-import { OrbitControls, useGLTF } from '@react-three/drei';
-import escultura2 from './img/ARCO.jpg';
+import escultura2 from './img/ARCO.webp';
 
 const Banner = () => {
   return (
-    <div className="banner-container">
-      <div className="info-container">
+    <div className="contenedor">
+      <div className="widget-1">
         <h2>Información de la Compañía</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam efficitur turpis at lacus cursus, eget lacinia
-          libero volutpat.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam efficitur turpis at lacus cursus, eget lacinia
-          libero volutpat.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam efficitur turpis at lacus cursus, eget lacinia
-          libero volutpat.
+          Google 3D Aesthetic es una empresa que te invita a explorar un nuevo horizonte artístico en su ecommerce. Encontrarás NFTs únicos diseñados por talentosos artistas 3D, que van desde cautivadoras esculturas digitales hasta innovadoras obras de arte. Sumérgete en la experiencia de coleccionar estas piezas digitales exclusivas o lleva tu favorita al mundo tangible imprimiéndola con materiales recomendados por el artista. Además, su selección se extiende más allá de los NFTs, ofreciendo una variada gama de pinturas, esculturas y moda que celebran la creatividad en todas sus formas. En Google 3D Aesthetic, la innovación y el arte convergen para ofrecerte una experiencia única.
         </p>
         <button>Leer más</button>
       </div>
-      <div className="image-container">
+      <div className="widget-2">
         <img src={escultura2} alt="escultura Img" />
       </div>
-    </div>
+    </div>   
   );
 };
-
 export default Banner;
-
