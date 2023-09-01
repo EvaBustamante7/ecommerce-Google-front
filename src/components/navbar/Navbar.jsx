3ff4ed1cb167d0a889b9e5c71e7edc1fa208f79d
Eva Bustamante <evabrodriguez7@gmail.com>
2023-09-01 14:27:17 +0200

    navbar transparente, con iconos, todo ok

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,28 +1,41 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { FaBars, FaTimes, FaShoppingCart } from 'react-icons/fa';
 import { BiSolidLogInCircle } from 'react-icons/bi';
-import Logo from '../logo/G3DA.svg'
-import './css/Navbar.css'
+import Logo from '../logo/G3DA.svg';
+import './css/Navbar.css';
 
 const Navbar = () => {
-    const [click, setClick] = useState(false)
-    const handleClick = () => setClick(!click)
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
 
-    const [color, setColor] = useState(false)
+    const [color, setColor] = useState(false);
     const changeColor = () => {
         if (window.scrollY >= 100) {
-            setColor(true)
+            setColor(true);
         } else {
-            setColor(false)
+            setColor(false);
         }
-    }
+    };
 
-    window.addEventListener('scroll', changeColor)
+    window.addEventListener('scroll', changeColor);
 
     return (
         <div className={color ? 'header header-bg' : 'header'}>
             <Link to='/'><img id='logo' src={Logo} alt="Logo G3da" /></Link>
+            <div className='nav-wrapper'>
+                <div className='hamburger' onClick={handleClick}>
+                    {click ? (<FaTimes size={25} style={{ color: 'black', margin: '0 10px'}} />) : <FaBars size={25} style={{ color: 'black', margin: '0 10px' }} />}
+                    <Link to='/login'>
+                        <BiSolidLogInCircle size={25} style={{ color: 'black', margin: '0 10px' }} />
+                    </Link>
+                    <Link to='/shop'>
+                        <FaShoppingCart size={25} style={{ color: 'black', margin: '0 10px' }}  />
+                    </Link>
+
+
+                </div>
+            </div>
             <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                 <li>
                     <Link to='/'>Home</Link>
@@ -36,25 +49,9 @@ const Navbar = () => {
                 <li>
                     <Link to='/contact'>Contact</Link>
                 </li>
-                <li>
-                    <Link to='/login'>
-                        <BiSolidLogInCircle size={25} style={{ color: 'black' }} />
-                    </Link>
-                </li>
-
-                <li>
-                    <Link to='/shop'>
-                        <FaShoppingCart size={25} style={{ color: 'black' }} />
-                    </Link>
-                </li>
-
             </ul>
-            <div className='hamburger' onClick={handleClick}>
-                {click ? (<FaTimes size={25} style={{ color: 'black' }} />) : <FaBars size={25} style={{ color: 'black' }} />}
-
-            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
