d75ab75393b80429c91c5864f4549135032b1971
Eva Bustamante <evabrodriguez7@gmail.com>
2023-09-01 11:31:05 +0200

    arreglo conjunto de navbar, banner, carrusel con rama de lili

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { FaBars, FaTimes } from 'react-icons/fa'
+import { FaBars, FaTimes, FaShoppingCart } from 'react-icons/fa';
+import { BiSolidLogInCircle } from 'react-icons/bi';
 import Logo from '../logo/G3DA.svg'
 import './css/Navbar.css'
 
@@ -36,12 +37,20 @@ const Navbar = () => {
                     <Link to='/contact'>Contact</Link>
                 </li>
                 <li>
-                    <Link to='/'>Shop</Link>
+                    <Link to='/login'>
+                        <BiSolidLogInCircle size={25} style={{ color: 'black' }} />
+                    </Link>
+                </li>
+
+                <li>
+                    <Link to='/shop'>
+                        <FaShoppingCart size={25} style={{ color: 'black' }} />
+                    </Link>
                 </li>
 
             </ul>
             <div className='hamburger' onClick={handleClick}>
-                {click ? (<FaTimes size={20} style={{ color: 'white' }} />) : <FaBars size={20} style={{ color: 'black' }} />}
+                {click ? (<FaTimes size={25} style={{ color: 'black' }} />) : <FaBars size={25} style={{ color: 'black' }} />}
 
             </div>
         </div>
