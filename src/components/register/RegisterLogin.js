763ef5178f6e5cfce3a44c7a18d0dadbe9eb310d
Eva Bustamante <evabrodriguez7@gmail.com>
2023-09-04 10:30:56 +0200

    pruebas

--- src/components/register/RegisterLogin.js
+++ src/components/register/RegisterLogin.js
@@ -1,12 +1,13 @@
 import React, { Fragment, useState } from 'react';
 import { urlLogin } from '../../Servicies/urls';
+import './css/Register.css'
 
 const Form = () => {
     const [datos, setDatos] = useState({
         user: '',
         pass: '',
         agreeTerms: ''
-       
+
     })
 
     const handleInputChange = (event) => {
@@ -20,15 +21,15 @@ const Form = () => {
 
     const enviarDatos = (event) => {
         event.preventDefault()
-        console.log('enviando datos...' + datos.user + ' ' + datos.pass )
+        console.log('enviando datos...' + datos.user + ' ' + datos.pass)
         var myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
-        
+
         //myHeaders.append("Cookie", "Authorization=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2ODA1Mjk0NjQsImV4cCI6MTY4MDUzMzA2NCwicm9sZXMiOlsiUk9MRV9VU0VSIl0sInVzZXJuYW1lIjoiY3Jpcy5tc2ZAZ21haWwuY29tIn0.rBvV96T2Y0tcPDpeybpQPVjHx7aY78wpPsnhlkIHKwTjgIWMCrHzWJkbqZATK2YenTN_dTAyKxYNYLN-5DB8BwyIei3nXfhgYJiWxc7M2lNq_gMib7hqoHRhk1uqcsWP_Ex9dfIqaWrQWKPo3fUd6Jlgu4QQJ-SlF6JZbTShpgEjf4fSOVrFmNc15bUwUxsctGigFrZ0YWTVYlEaDuFJj1bOtI47vbPRSLEHAsX88rdpCQOikrxEx545BRsmpoB9-YBxpuRAc7RyMVcEF08WY10h1VHSTsSvNps9PNImLMd95FZuRPpE5DL511WvlwhY4ytpyfXq5UQy9_5QyJGxhg; BEARER=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2ODA1Mjk0NjQsImV4cCI6MTY4MDUzMzA2NCwicm9sZXMiOlsiUk9MRV9VU0VSIl0sInVzZXJuYW1lIjoiY3Jpcy5tc2ZAZ21haWwuY29tIn0.rBvV96T2Y0tcPDpeybpQPVjHx7aY78wpPsnhlkIHKwTjgIWMCrHzWJkbqZATK2YenTN_dTAyKxYNYLN-5DB8BwyIei3nXfhgYJiWxc7M2lNq_gMib7hqoHRhk1uqcsWP_Ex9dfIqaWrQWKPo3fUd6Jlgu4QQJ-SlF6JZbTShpgEjf4fSOVrFmNc15bUwUxsctGigFrZ0YWTVYlEaDuFJj1bOtI47vbPRSLEHAsX88rdpCQOikrxEx545BRsmpoB9-YBxpuRAc7RyMVcEF08WY10h1VHSTsSvNps9PNImLMd95FZuRPpE5DL511WvlwhY4ytpyfXq5UQy9_5QyJGxhg");
 
         var raw = JSON.stringify({
-            "email":datos.user,
-            "password":datos.pass,
+            "email": datos.user,
+            "password": datos.pass,
 
         });
         // var raw = JSON.stringify({
@@ -41,11 +42,11 @@ const Form = () => {
             headers: myHeaders,
             body: raw,
             redirect: 'follow',
-           
+
         };
 
         function saveToken(result) {
-           
+
             console.log(result)
             console.log("estoy en savetoken despues del consolelog result")
             //localStorage.setItem('token', token);
@@ -55,38 +56,31 @@ const Form = () => {
         fetch(urlLogin + "/token/register", requestOptions)
             //.then(response => console.log(response))
             .then(response => response.text())
-             .then(result => saveToken(result))
+            .then(result => saveToken(result))
             .catch(error => console.log('error', error));
     }
-   
-
-
-
-
-
 
     return (
         <Fragment>
-            <h1>Formulario de Registro</h1>
-            <form className="row" onSubmit={enviarDatos}>
-                <div className="form-group p-3">
-                    <input type="text" placeholder="User" className="form-control" onChange={handleInputChange} name="user"></input>
-                </div>
-                <div className="form-group p-3">
-                    <input type="text" placeholder="Password" className="form-control" onChange={handleInputChange} name="pass"></input>
-                </div>
-               
-                
-                <div className="form-group p-3">
-                    <label>
-                        <input type="checkbox" onChange={handleInputChange} name="agreeTerms" />
-                        Acepto los términos y condiciones
-                    </label>
-                </div>
-                <div className="form-group p-3">
-                    <button type="submit" className="btn btn-primary">Enviar</button>
-                </div>
-            </form>
+            <div className='register'>
+                <form className="row" onSubmit={enviarDatos}>
+                    <div className="form-group p-3">
+                        <input type="text" placeholder="User" className="form-control" onChange={handleInputChange} name="user"></input>
+                    </div>
+                    <div className="form-group p-3">
+                        <input type="text" placeholder="Password" className="form-control" onChange={handleInputChange} name="pass"></input>
+                    </div>
+                    <div className="form-group p-3">
+                        <label>
+                            <input type="checkbox" onChange={handleInputChange} name="agreeTerms" />
+                            Acepto los términos y condiciones
+                        </label>
+                    </div>
+                    <div className="form-group p-3">
+                        <button type="submit" className="btn btn-primary">Enviar</button>
+                    </div>
+                </form>
+            </div>
             {/*<ul>
                 <li>{datos.nombre}</li>
                 <li>{datos.apellido}</li>
