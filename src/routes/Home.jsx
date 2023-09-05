a874e3f86252574fa2b5bf1c8414b4b675b8283c
Zoraida Manzanares <zoraidamanzanaresj@gmail.com>
2023-09-05 11:26:15 +0200

    logo y colores

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -7,12 +7,14 @@ import  imagen from '../components/assets/ARCO.webp'
 import  imagen1 from '../components/assets/imagenes1.jpg'
 import  imagen2 from '../components/assets/LlFD.gif'
 import ModelViewer from '../components/modelviewer/model_viewer'
+import LogoViewer from '../components/logoViewer/logoViewer'
 
 
 const Home = () => {
   return (
     <>
     <Navbar/>
+    <LogoViewer/>
     <Banner
      title="Google 3D Aesthetic "
      text="Google 3D Aesthetic es una empresa que te invita a explorar un nuevo horizonte artístico en su ecommerce. Encontrarás NFTs únicos diseñados por talentosos artistas 3D, que van desde cautivadoras esculturas digitales hasta innovadoras obras de arte. Sumérgete en la experiencia de coleccionar estas piezas digitales exclusivas o lleva tu favorita al mundo tangible imprimiéndola con materiales recomendados por el artista. Además, su selección se extiende más allá de los NFTs, ofreciendo una variada gama de pinturas, esculturas y moda que celebran la creatividad en todas sus formas. En Google 3D Aesthetic, la innovación y el arte convergen para ofrecerte una experiencia única."
