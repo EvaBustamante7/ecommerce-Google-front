76ba36b5ac7465afca1bea59955cf1d74badeb3d
Liliana <garciasevericheliliana@gmail.com>
2023-09-05 12:16:49 +0200

    modificado el home,model_viewer,logoViewer

--- src/components/logoViewer/logoViewer.js
+++ src/components/logoViewer/logoViewer.js
@@ -7,9 +7,8 @@ function LogoViewer() {
     <div>
       <div className="contenedor">
         <div className="widget-1">
-          <h2>damelo ya</h2>
-          <p></p>
-          <button>Leer más</button>
+          <h2>Google 3D Aesthetic</h2>
+          <p>Te invita a explorar su ecommerce. Donde encontrarás NFTs únicos diseñados por talentosos artistas 3D, que van desde cautivadoras esculturas digitales hasta innovadoras obras de arte.  ofreciendo una variada gama de pinturas y esculturas que desarrollan la creatividad en todas sus formas.</p>
         </div>
         <div className="widget-2">
           <iframe
