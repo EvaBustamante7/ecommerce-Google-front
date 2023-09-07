f81b4cf73758a68bffa11e2c2ccfd70650f53211
Eva Bustamante <evabrodriguez7@gmail.com>
2023-09-07 13:48:05 +0200

    prueba

--- src/components/products/Productslist.js
+++ src/components/products/Productslist.js
@@ -28,8 +28,8 @@ const ProductList = () => {
         {products.map(product => (
           <div className='product' key={product.id}>
              <h3>{product.name}</h3>
-            <img src={`https://127.0.0.1:8000/uploads/brochures/${product.image}`} alt={product.name} />
-            <img src={`https://127.0.0.1:8000/uploads/brochures/${product.qr}`} alt={`${product.name}QR`} />
+            <img src={`http://127.0.0.1:8000/uploads/brochures/${product.image}`} alt={product.name} />
+            <img src={`http://127.0.0.1:8000/uploads/brochures/${product.qr}`} alt={`${product.name}QR`} />
             
             <p>Precio: {product.price} €</p>
             <button onClick={() => addToCart(product)}>Añadir al carrito</button>
