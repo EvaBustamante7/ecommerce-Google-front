0ec2004350c752bf173068d2e5296a53130e71ae
Eva Bustamante <evabrodriguez7@gmail.com>
2023-09-10 16:40:26 +0200

    contacto con css, todos los formularios responsives, test terminados y pasan todos

--- src/components/contact_form/Contact_form.jsx
+++ src/components/contact_form/Contact_form.jsx
@@ -1,6 +1,7 @@
-import React, { useRef } from 'react';
-import './css/Contact_form.css';
+import React, { Fragment, useRef } from 'react';
+import {  FaDotCircle } from 'react-icons/fa';
 import ReCAPTCHA from 'react-google-recaptcha';
+import './css/Contact_form.css'; 
 
 const ContactForm = () => {
   const captcha = useRef(null);
@@ -9,35 +10,63 @@ const ContactForm = () => {
     e.preventDefault();
     if (captcha.current.getValue()) {
       alert('¡Gracias, tu mensaje ha sido enviado!');
-      captcha.current.reset(); // Reiniciar el reCAPTCHA
-      e.target.reset(); // Reiniciar el formulario después del envío
+      captcha.current.reset(); 
+      e.target.reset(); 
     } else {
       alert('Por favor, verifica que eres humano');
     }
   };
 
   return (
-    <div className='form'>
-      <form id='contactForm' onSubmit={handleSubmit}>
-        <label>Nombre</label>
-        <input type='text' />
-        <label>Email</label>
-        <input type='email' />
-        <label>Título</label>
-        <input type='text' />
-        <label>Mensaje</label>
-        <textarea rows='6' placeholder='Escribe aquí tu mensaje' />
-        <div className='recaptcha'>
-          <ReCAPTCHA
-            ref={captcha}
-            sitekey='6LcHSjEmAAAAADpYYDwgZFzzNw5nBlrt5VfXFiVc'
-          />
+    <Fragment>
+      <div className='container'>
+        <div className='screen__contact'>
+          <div className='screen__content'>
+            <form className='contact' onSubmit={handleSubmit}>
+              <div className='login__field'>
+                <span className='login__icon'>
+                  <FaDotCircle />
+                </span>
+                <input type='text' placeholder="Nombre" className='login__input' />
+              </div>
+              <div className='login__field'>
+                <span className='login__icon'>
+                  <FaDotCircle />
+                </span>
+                <input type='email' placeholder="Email" className='login__input' />
+              </div>
+              <div className='login__field'>
+              <span className='login__icon'>
+                  <FaDotCircle />
+                </span>
+                <input type='text' placeholder="Título"  className='login__input' />
+              </div>
+              <div className='login__field'>
+              <span className='login__icon'>
+                  <FaDotCircle />
+                </span>
+                <textarea rows='6' placeholder='Escribe aquí tu mensaje' className='login__input' />
+              </div>
+              <div className='recaptcha'>
+                <ReCAPTCHA
+                  ref={captcha}
+                  sitekey='6LcHSjEmAAAAADpYYDwgZFzzNw5nBlrt5VfXFiVc'
+                />
+              </div>
+              <button className='login__submit' type='submit'>
+                Enviar
+              </button>
+            </form>
+          </div>
+          <div className='screen__background'>
+            <span className='screen__background__shape screen__background__shape1'></span>
+            <span className='screen__background__shape screen__background__shape2'></span>
+            <span className='screen__background__shape screen__background__shape3'></span>
+            <span className='screen__background__shape screen__background__shape4'></span>
+          </div>
         </div>
-        <button className='btn' type='submit'>
-          Enviar
-        </button>
-      </form>
-    </div>
+      </div>
+    </Fragment>
   );
 };
 
