0414a48a9a7c94d442c5035afd892b4c9ba73e6c
Eva Bustamante <evabrodriguez7@gmail.com>
2023-09-10 20:10:28 +0200

    limpiando la main

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import Contact from './routes/Contact';
 import Inmersiva from './routes/Inmersiva';
-import Products from './routes/Products';
+
 import Obj3d from './routes/Obj3d'
 import About from './components/about/About';
 import Cookies from './components/cookies/Cookies';
 import Privacy from './components/privacy-policy/Privacy';
-import Productdetail from './routes/Productdetail';
+
 import Loger from './routes/Loger';
 import Regroute from './routes/Regroute';
 import ScrollToTop from './Scroll/ScrollToTop';
@@ -22,8 +22,7 @@ function App() {
         <Route path='/' element={<Home/>} />
         <Route path='/contact' element={<Contact/>} />
         <Route path='/inmersiva' element={<Inmersiva/>} />
-        <Route path='/products' element={<Products/>} />
-        <Route path='/products/:id' element={<Productdetail/>} />
+       
         <Route path='/register' element={<Regroute/>} />
         <Route path='/objeto' element={<Obj3d/>} />
         <Route path='/about' element={<About/>} />
