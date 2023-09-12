4a36b8d61ca06dc6406453b40c4410b3b1666438
Eva Bustamante <evabrodriguez7@gmail.com>
2023-09-12 15:31:09 +0200

    quitar las urls de service

--- src/Servicies/urls.js
+++ src/Servicies/urls.js
@@ -1,5 +1,3 @@
-export const urlApi = 'https://cristinamaser.com/api/invitations';
-export const urlImg = 'https://cristinamaser.com';
 export const urlLogin = 'https://127.0.0.1:8000';
 
 //export default urlApi;
